545005304bef6253419fb536220c7e36df183f03
Andre <andrescat98@gmail.com>
2025-03-26 19:58:38 +0100

    supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+const supabaseUrl = 'https://vyhzhwvrivovhspyefpb.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+
+if (!supabaseKey) {
+  throw new Error('La chiave SUPABASE_KEY non è definita. Verifica le variabili d\'ambiente.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
