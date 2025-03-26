b722bb56f440b9483c514300019c543096d6c3e8
Andre <andrescat98@gmail.com>
2025-03-26 18:13:43 +0100

    supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,13 @@
-// lib/supabase.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Ottieni le variabili di ambiente
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Verifica che le variabili di ambiente siano definite
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Le variabili di ambiente Supabase non sono configurate correttamente.')
+// Controlla se le variabili d'ambiente sono presenti
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL or key is not defined in environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
