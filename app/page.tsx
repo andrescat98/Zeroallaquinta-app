1dd912cb8d2906599e814057882324ebd1f22cd9
andrescat98 <andrescat98@gmail.com>
2025-03-27 16:55:17 +0100

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link'
 import { useState, useEffect } from "react"
-import { supabase } from "../lib/supabase"  // Assicurati che il path sia corretto
+import { supabase } from '@/utils/supabase/server'
 import { useRouter } from 'next/navigation' // Usa il nuovo hook per l'App Router
 
 export default function LoginPage() {
