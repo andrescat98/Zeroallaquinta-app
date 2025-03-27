e76e17b3fd92cc40cf310b704a3d6d6050e297dc
andrescat98 <andrescat98@gmail.com>
2025-03-27 16:57:36 +0100

    asd

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from '@/utils/supabase/server'
+import { supabase } from "@/lib/server";
 import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
